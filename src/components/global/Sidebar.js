3d81156cd37ee006ad8f16d400829a78999290da
caleb <ogundelecaleb13@gmail.com>
2024-12-04 17:22:00 +0100

    commit

--- src/components/global/Sidebar.js
+++ src/components/global/Sidebar.js
@@ -62,6 +62,7 @@ const Sidebar = ({ isSidebarOpen, onClose, profileData }) => {
               stroke-linejoin="round"
               d="M6 18L18 6M6 6l12 12"
             />
+            
           </svg>
         </button>
       </div>
